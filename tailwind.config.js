8bd90cfcce8cd531f029e88cd0e08d22d3f6e37a
Zaid Kajee <zaid26@gmail.com>
2023-11-23 13:53:41 +0200

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   content: ["./src/*.html"],
   theme: {
     extend: {
-      colors: {
-        'daily-dev-tips': "#F89283"
+      fontFamily:{
+        'primary': ['Poppins']
       }
     },
   },
